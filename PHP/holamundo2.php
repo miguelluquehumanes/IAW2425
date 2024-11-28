563865688704c8c1b74aa026b8ede00e0a8b087d
Miguel Luque <miguelluque05@iesamachado.org>
2024-11-28 14:20:00 +0100

    clase 28 nov

---
+++ PHP/holamundo2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensaje al mundo</title>
+</head>
+<body>
+    <?php
+        $mensaje1="Hola mundo";
+        $mensaje2=" desde PHP";
+        echo"<h1>$mensaje1$mensaje2</h1>"
+    ?>
+</body>
+</html>
---
+++ PHP/operadoresNumericos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores numericos</title>
+</head>
+<body>
+    <?php
+
+        $suma = 1 + 1;
+        $multiplicacion = 100 * 35;
+        $division = 100 / 50;
+        $resta = 100 - 40;
+
+        echo "La suma 1 + 1 da como resultado: $suma <br>";
+        echo "La multiplicacion 100 * 35 da como resultado: $multiplicacion <br>";
+        echo "La división 100 / 50 da como resultado: $division <br>";
+        echo "La resta 100 - 40 da como resultado: $resta";
+    ?>
+</body>
+</html>
